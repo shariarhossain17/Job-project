cccdd95e152c2c52ed8b8dc70a576443f96232b4
shariarhossain23e <shariarhossain23@gamil.comm>
2022-06-28 04:07:01 +0600

    fake data

---
+++ src/Hooks/Product.js
@@ -0,0 +1,37 @@
+
+const Product = () => {
+    const product = [
+        {
+         name: "Audrino Nano",
+          price: "20,70",
+          picture: "https://i.ibb.co/N9f59xC/Arduino-Nano.jpg",
+          pictur2: "https://i.ibb.co/R2TfRWX/arduino-nano-board.jpg",
+          pictur3: "https://i.ibb.co/R2TfRWX/arduino-nano-board.jpg",
+          pictur4: "https://i.ibb.co/v39FyVd/download.jpg",
+          desc: "The Arduino Nano is a small, complete, and breadboard-friendly board based on the ATmega328 (Arduino Nano 3.x). It has more or less the same functionality of the Arduino Duemilanove, but in a different package."
+        },
+        {
+         name: "Gps sensors",
+          price: "15,90",
+          picture: "https://i.ibb.co/M6vfVdR/u-blox-neo-6m-gps-module-robotics-bangladesh.jpg",
+          pictur2: "https://i.ibb.co/PDQ6J5Y/pin-description.jpg",
+          pictur3: "https://i.ibb.co/mBdQLxs/neo-6m-gps-2.png",
+          pictur4: "https://i.ibb.co/BnTzvsB/download.jpg",
+          desc: "GPS sensors are receivers with antennas that use a satellite-based navigation system with a network of 24 satellites in orbit around the earth to provide position, velocity, and timing information."
+        },
+        {
+         name: "Gsm sensors",
+          price: "25,30",
+          picture: "https://i.ibb.co/BnTzvsB/download.jpg",
+          pictur2: "https://i.ibb.co/hY6kRmX/download-1.jpg",
+          pictur3: "https://i.ibb.co/0htp7yH/download-2.jpg",
+          pictur4: "https://i.ibb.co/VW4ZSw0/17d5aaa6e3354048b0f2932cedac628c.jpg",
+          desc: "In GSM system architecture, it includes different elements, which are frequently known as the core system/network. Here, it is basically a data network including a variety of units to provide"
+        },
+      ]
+
+     
+    return  product;
+};
+
+export default Product;
